aee7ad492a332948a4eaf6370d3b2f6909eb519b
Alexander Pavlov <workboxpavlov@yandex.ru>
2022-04-06 20:35:41 +0300

    first console application

---
+++ chapter1/lesson1/js/script.js
@@ -0,0 +1,19 @@
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+let questionOne = prompt("Один из последних просмотренных фильмов?", "");
+let questionTwo = +prompt("На сколько оцените его?", "");
+let questionThree = prompt("Один из последних просмотренных фильмов?", "");
+let questionFour = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[questionOne] = questionTwo;
+personalMovieDB.movies[questionThree] = questionFour;
+
+console.log(personalMovieDB);
+
