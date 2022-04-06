5f58499a4ac65b96591350fad6f6e5b87b892117
Alexander Pavlov <workboxpavlov@yandex.ru>
2022-04-06 21:53:59 +0300

    update app

--- chapter1/lesson1/js/script.js
+++ chapter1/lesson1/js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB = {
@@ -7,13 +9,29 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 }
-let questionOne = prompt("Один из последних просмотренных фильмов?", "");
-let questionTwo = +prompt("На сколько оцените его?", "");
-let questionThree = prompt("Один из последних просмотренных фильмов?", "");
-let questionFour = +prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[questionOne] = questionTwo;
-personalMovieDB.movies[questionThree] = questionFour;
+if (personalMovieDB.count < 10) { 
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else 
+    console.log("Произошла ошибка");
+
+for (let i = 0;i < 2; i++) {
+    let questionOne = prompt("Один из последних просмотренных фильмов?", ""),
+        questionTwo = +prompt("На сколько оцените его?", "");
+
+    if  (!questionOne || questionOne.length > 50 || questionOne == "") {
+        questionOne = prompt("Один из последних просмотренных фильмов?", "");
+    } 
+    if  (!questionTwo || questionOne.length > 50 || questionTwo == "") {
+        questionTwo = prompt("На сколько оцените его?", "");
+    } 
+
+        personalMovieDB.movies[questionOne] = questionTwo;
+}
 
 console.log(personalMovieDB);
 
